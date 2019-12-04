08b40e1d746c4cfa589ce1411c33dd511d0f8021
Sagnik Routh <Sagnik Routh>
2019-12-05 00:51:19 +0800

    Initial commit for string accumulator

---
+++ stringaccumulator/src/main/java/com/sagnik/stringaccumulator/StringAccumulator.java
@@ -0,0 +1,81 @@
+package com.sagnik.stringaccumulator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.StringUtils;
+
+
+
+public class StringAccumulator {
+
+	public static void main(String[] args) {
+		try {
+			if(args.length>0) {
+				System.out.println("Input String:"+args[0]);
+				System.out.println("Sum of inputted numbers:"+add(args[0]));
+			}else {
+				System.out.println("No arguments passed, so sum is 0");
+			}
+		}catch(Exception ex) {
+			System.err.println("Exception while adding numbers:"+ex.getMessage());
+		}
+		System.out.println("**************************************************");
+	}
+
+	private static Integer add(String str) throws Exception{
+		if(StringUtils.isEmpty(str)) {
+			return 0;//return 0 sum in case of null or empty string
+		}
+		String delim=",";
+		if(StringUtils.contains(str, "\n")) {
+			String firstLine=str.split("\n")[0];
+			if(StringUtils.isNumeric(firstLine)) {//check if first line is delimeter or actual number
+				str=str.replaceAll("\\n", ",");//replace all new line delimeters with comma			
+			}else {//first line is the optional delimeter or list of delimeters
+				str=str.substring(str.indexOf("\n")+1);//separate out 1st line 
+																	   //and get actual number list	
+				StringBuilder delimSb = formatMultiDelimeters(firstLine);
+				//place the entire delimeter list in the delim variable separated by |to be passed on
+				//to summation method
+				delim = delimSb.substring(0,delimSb.length()-1);				
+			}
+		}
+		return sumOfIntegersFromString(str, delim);
+	}
+
+	private static StringBuilder formatMultiDelimeters(String firstLine) {
+		StringBuilder delimSb = new StringBuilder();
+		List<String> delimeterList=Arrays.asList(firstLine.split("\\|"));
+		for(String delims:delimeterList) {
+			StringBuilder sb = new StringBuilder();
+			delims.chars().distinct().forEach(c -> sb.append((char) c));//remove duplicates from delimeters
+			delimSb.append(sb);
+			delimSb.append("|");
+		}
+		return delimSb;
+	}
+
+	private static Integer sumOfIntegersFromString(String str, String delim) throws Exception {
+		Integer sum=0;
+		List<Integer> convertedNoList = Stream.of(str.split(delim)).filter(StringUtils::isNotBlank)
+				  .map(String::trim)
+				  .map(Integer::parseInt)
+				  .collect(Collectors.toList());
+		//throw exception in case of negative integers
+		List<Integer> negativeList = convertedNoList.stream().filter(i->i<0).collect(Collectors.toList());
+		if(!negativeList.isEmpty()) {
+			String negativeNos=String.join(", ",negativeList.stream().map(i->i.toString()).
+					collect(Collectors.toList()));
+			throw new Exception("Negative numbers not allowed:"+negativeNos);
+                    
+		}
+		//do actual summation
+		sum=convertedNoList.stream().filter(i->i<=1000) // filter out all numbers only less than 1000
+				.collect(Collectors.summingInt(Integer::intValue));
+		return sum;
+	}
+
+}
---
+++ stringaccumulator/src/test/java/com/sagnik/stringaccumulator/StringAccumulatorTest.java
@@ -0,0 +1,58 @@
+package com.sagnik.stringaccumulator;
+
+import org.junit.Test;
+
+import com.sagnik.stringaccumulator.StringAccumulator;
+
+public class StringAccumulatorTest {
+	
+	@Test
+	public void testAddIntsCommaSeparated() {
+		String emptyargs[] = {""};
+		StringAccumulator.main(emptyargs);
+		
+		String singlearg[] = {"4"};
+		StringAccumulator.main(singlearg);
+		
+		String args[] = {"2,5,7,12"};
+		StringAccumulator.main(args);
+		
+		String args1[] = {"2,,5,,7,,3,1001"};
+		StringAccumulator.main(args1);		
+		
+	}
+
+	@Test
+	public void testAddIntsNewLineSeparated() {
+		String args[] = {"2\n5,7\n12"};
+		StringAccumulator.main(args);
+	}
+	
+	@Test
+	public void testAddIntsMultiDelims() {
+		String args[] = {"\\*|%\n1*2%3"};
+		StringAccumulator.main(args);
+		
+		String args1[] = {"\\%|\\*\n1****2%%%%3"};
+		StringAccumulator.main(args1);
+		
+		String args2[] = {"\\%%%%|\\********\n1****2%%%%3"};
+		StringAccumulator.main(args2);
+	}
+	
+	@Test
+	public void testAddIntsMultiDelims2() {
+		String args[] = {";\n1;2;23"};
+		StringAccumulator.main(args);
+		
+		String args1[] = {":::::::\n::1:::4:::23"};
+		StringAccumulator.main(args1);	
+		
+	}
+	
+	@Test
+	public void testAddIntsNegative() {
+		String negativeArgs[] = {"2,5,-7,3,-15"};
+		StringAccumulator.main(negativeArgs);
+	}
+}
